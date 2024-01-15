6c48188da300f384e4ca425d69c6b9612938b3a8
Cisse Anzoumana <kestyraf@gmail.com>
2024-01-15 12:56:00 +0000

    updated event preview task time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('task:event-preview')
-            ->everyTenSeconds()
+            ->everyTwoHours()
             ->appendOutputTo('scheduler.log');
     }
 
